770d22174d903ea7475475f82f5f899cf62a6684
innosday <162128735+innosday@users.noreply.github.com>
2025-05-21 11:23:29 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,70 @@
-import pygame,sys
+import pygame,sys,random
 
 class Boxs:
-    def __init__(self,screen:pygame.Surface):
+    def __init__(self,screen:pygame.Surface,size=100):
         self.__screen = screen
         screen_width,screen_height = screen.get_size()
         self.__Boxs = []
-        for low in range(0,screen_height,100):
+        self.__botPos = {"start":None,"end":None}
+        for low in range(0,screen_height,size):
             lowList = []
-            for high in range(0,screen_width,100):
-                lowList.append({"rect":pygame.Rect(high,low,100,100),"center":(high+50,low+50)})
+            for high in range(0,screen_width,size):
+                lowList.append({"rect":pygame.Rect(high,low,size,size),"center":(high+size//2,low+size//2),"class":None,"perant":None,"g":None,"h":None})
             self.__Boxs.append(lowList)
     @property
     def Boxs(self) -> list[list[dict]]:
         return self.__Boxs
+    
+    @property
+    def botPos(self) -> dict:
+        return self.__botPos
+    
+    def getBoxCenter(self,pos:tuple[int,int]) -> tuple:
+        return self.__Boxs[pos[0]][pos[1]]["center"]
+    def setBoxClass(self,pos:tuple[int,int],object = None):
+        """start,end,wall"""
+        self.__Boxs[pos[0]][pos[1]]["class"] = object
+        if object in ["start","end"]:
+            self.__botPos[object] = pos
+    def getBoxClass(self,pos:tuple[int,int]):
+        return self.__Boxs[pos[0]][pos[1]]['class']
 
     def update(self):
         for box in self.__Boxs:
             for inbox in box:
-                pygame.draw.rect(self.__screen,(255,0,0),inbox["rect"],1)
-                pygame.draw.circle(self.__screen,(0,255,0),inbox["center"],4)
+                if inbox["class"] == "wall":
+                    pygame.draw.rect(self.__screen,(10,10,10),inbox["rect"])
+                elif inbox["class"] == "start":
+                    pygame.draw.rect(self.__screen,(255,0,0),inbox["rect"])
+                elif inbox["class"] == "end":
+                    pygame.draw.rect(self.__screen,(0,255,0),inbox["rect"])
+                else:
+                    pygame.draw.rect(self.__screen,(10,10,10),inbox["rect"],1)
+                pygame.draw.circle(self.__screen,(0,0,255),inbox["center"],4)
+
+class Bot:
+    def __init__(self,screen:pygame.Surface,grid:Boxs):
+        print("영우 배달 시작!")
+        self.__screen = screen
+        self.__Pos = grid.getBoxCenter(grid.botPos["start"])
+    
+    def update(self):
+        pass
+
 class Game:
     def __init__(self):
         pygame.init()
-        self.__screen = pygame.display.set_mode((500, 700))
-        self.__grid=Boxs(self.__screen)
+        self.screen = pygame.display.set_mode((500, 700))
+        pygame.display.set_caption("영우 배달 게임")
+        self.grid=Boxs(self.screen)
+        
+        for _ in range(7):
+            randomPos = (random.randrange(0,self.screen.get_height()//100-1),random.randrange(0,self.screen.get_width()//100-1))
+            self.grid.setBoxClass(randomPos,"wall")
+        self.grid.setBoxClass((6,0),"start")
+        self.grid.setBoxClass((0,4),"end")
+
+        self.bot = Bot(self.screen,self.grid)
 
     def EventManager(self):
         for event in pygame.event.get():
@@ -32,9 +73,9 @@ class Game:
                 sys.exit()
 
     def updates(self):
-        self.__screen.fill((255, 255, 255))
-        self.__grid.update()
-
+        self.screen.fill((255, 255, 255))
+        self.grid.update()
+        self.bot.update()
         pygame.display.update()
 
     def main(self):
