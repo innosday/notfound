7bf4f9283470a5bfc793137c071b70274a9061a7
innosday <imylove2007@gmail.com>
2025-05-28 21:15:11 +0900

    A* 알고리즘 모듈화 완성

---
+++ Node.py
@@ -0,0 +1,179 @@
+import random
+
+class Boxs:
+    def __init__(self,pos:tuple[int,int]):
+        self.__y,self.__x = pos
+        self.__g = 0
+        self.__h = 0
+        self.__perant = None
+        self.__box_class = None
+
+    @property
+    def x(self):
+        return self.__x
+    @property
+    def y(self):
+        return self.__y
+    @property
+    def f(self):
+        return self.__g + self.__h
+    @property
+    def g(self):
+        return self.__g
+    @property
+    def h(self):
+        return self.__h
+    @property
+    def perant(self):
+        return self.__perant
+    @property
+    def box_class(self):
+        return self.__box_class
+    
+    @g.setter
+    def g(self,g:int):
+        self.__g = g
+    @h.setter
+    def h(self,h:int):
+        self.__h = h
+    @perant.setter
+    def perant(self,perant:tuple[int,int]):
+        self.__perant = perant
+    @box_class.setter
+    def box_class(self,Class:str):
+        self.__box_class = Class
+
+class Grid:
+    def __init__(self,size:tuple[int,int]):
+        self.__grid = [[Boxs((i,j)) for j in range(size[1])] for i in range(size[0])]
+        self.__bottomLeft = {"y":size[1]-1,"x":0}
+        self.__topRight = {"y":0,"x":size[0]-1}
+
+    @property
+    def BottomLeft(self) -> tuple[int,int]:
+        return self.__bottomLeft
+    
+    @property
+    def TopRight(self) -> tuple[int,int]:
+        return self.__topRight
+      
+    def getGrid(self,pos) -> Boxs:
+        return self.__grid[pos[0]][pos[1]]
+    
+    def showGrid(self):
+        for i in self.__grid:
+            for j in i:
+                print("%5s"%j.box_class,end=" ")
+            print()
+            
+    def showDetailGrid(self):
+        for i in self.__grid:
+            for j in i:
+                print(f"({j.y},{j.x}) g:{j.g} h:{j.h} f:{j.f} perant:{j.perant} class:{j.box_class}",end=" | ")
+            print()
+        print("-" * 20)
+
+    def setBoxClass(self,pos:tuple[int,int],Class:str):
+        self.__grid[pos[0]][pos[1]].box_class = Class
+
+    @property
+    def getStartPos(self) -> tuple[int,int]:
+        for i in self.__grid:
+            for j in i:
+                if j.box_class == "start":
+                    return (j.y,j.x)
+    @property
+    def getEndPos(self) -> tuple[int,int]:
+        for i in self.__grid:
+            for j in i:
+                if j.box_class == "end":
+                    return (j.y,j.x)
+
+class Node:
+    def __init__(self,size:tuple[int,int],end:tuple[int,int],start:tuple[int,int]):
+        self.grid = Grid(size)
+        self.grid.setBoxClass(start,"start")
+        self.grid.setBoxClass(end,"end")
+
+        self.openlist = [self.grid.getStartPos]
+        self.closelist = []
+        self.finallist = []
+        self.cur = self.openlist[0]
+
+    def setWall(self,pos:tuple[int,int]):
+        self.grid.setBoxClass(pos,"wall")
+        
+    def OpenListAdd(self,y,x):
+        pos = (y,x)
+        # print(self.openlist)
+        if x >= self.grid.BottomLeft["x"] and x <= self.grid.TopRight["x"] and y <= self.grid.BottomLeft["y"] and y >= self.grid.TopRight["y"] and self.grid.getGrid(pos).box_class != "wall" and pos not in self.closelist:
+            NegiborNode = pos
+            MoveCost = self.grid.getGrid(self.cur).g + (10 if self.grid.getGrid(self.cur).y - y == 0 or self.grid.getGrid(self.cur).x - x == 0 else 14)
+
+            if MoveCost < self.grid.getGrid(NegiborNode).g or NegiborNode not in self.openlist:
+                self.grid.getGrid(NegiborNode).g = MoveCost
+                self.grid.getGrid(NegiborNode).h = (abs(NegiborNode[1] - self.grid.getEndPos[1]) + abs(NegiborNode[0] - self.grid.getEndPos[0])) * 10
+                self.grid.getGrid(NegiborNode).perant = self.cur
+                self.openlist.append(NegiborNode)
+
+    def findNode(self):
+        while self.openlist:
+            self.cur = self.openlist[0]
+            for i in self.openlist:
+                if self.grid.getGrid(i).f <= self.grid.getGrid(self.cur).f and self.grid.getGrid(i).h < self.grid.getGrid(self.cur).h:
+                    self.cur = i
+
+            self.openlist.remove(self.cur)
+            self.closelist.append(self.cur)
+
+            if self.cur == self.grid.getEndPos:
+                TargetCur = self.grid.getEndPos
+                while TargetCur != self.grid.getStartPos:
+                    \
+                    self.finallist.append(TargetCur)
+                    TargetCur = self.grid.getGrid(TargetCur).perant
+                self.finallist.append(self.grid.getStartPos)
+                self.finallist.reverse()
+                for i in self.finallist:
+                    print(i,end=" -> ")
+                print("end")
+                return self.finallist
+            self.OpenListAdd(self.grid.getGrid(self.cur).y-1, self.grid.getGrid(self.cur).x)
+            self.OpenListAdd(self.grid.getGrid(self.cur).y, self.grid.getGrid(self.cur).x+1)
+            self.OpenListAdd(self.grid.getGrid(self.cur).y+1, self.grid.getGrid(self.cur).x)
+            self.OpenListAdd(self.grid.getGrid(self.cur).y, self.grid.getGrid(self.cur).x-1)
+
+
+
+
+    # grid.showDetailGrid()
+
+class TestNode:
+    def __init__(self):
+        self.ranPos = []
+        self.size = 5
+    def randomPos(self,start,end) -> int:
+        ran = (random.randint(start,end),random.randint(start,end))
+        while True:
+            if ran not in self.ranPos:
+                self.ranPos.append(ran)
+                break
+            else:
+                ran = (random.randint(start,end),random.randint(start,end))
+
+    def test(self):
+        for _ in range(3):
+            self.ranPos = []
+            for i in range(self.size+2):
+                self.randomPos(0,self.size-1)
+            print(self.ranPos)
+            node = Node((self.size,self.size),self.ranPos[0],self.ranPos[1])
+            for i in self.ranPos[2:]:
+                node.setWall(i)
+            node.grid.showGrid()
+            nodelist = node.findNode()
+            # print("Final Path:",nodelist) 
+            print("-" * 20)
+
+
+
--- main.py
+++ main.py
@@ -6,10 +6,11 @@ class Boxs:
         screen_width,screen_height = screen.get_size()
         self.__Boxs = []
         self.__botPos = {"start":None,"end":None}
+        self.__size = size
         for low in range(0,screen_height,size):
             lowList = []
             for high in range(0,screen_width,size):
-                lowList.append({"rect":pygame.Rect(high,low,size,size),"center":(high+size//2,low+size//2),"class":None,"perant":None,"g":None,"h":None})
+                lowList.append({"rect":pygame.Rect(high,low,size,size),"center":(high+size//2,low+size//2),"class":None,"perant":None,"g":0,"h":0,"f":0})
             self.__Boxs.append(lowList)
     @property
     def Boxs(self) -> list[list[dict]]:
@@ -19,7 +20,12 @@ class Boxs:
     def botPos(self) -> dict:
         return self.__botPos
     
-    def getBoxCenter(self,pos:tuple[int,int]) -> tuple:
+    @property
+    def size(self) -> int:
+        return self.__size
+   
+    def getBoxCenter(self,pos:tuple[int,int]) -> tuple[int,int]:
+        """return center"""
         return self.__Boxs[pos[0]][pos[1]]["center"]
     def setBoxClass(self,pos:tuple[int,int],object = None):
         """start,end,wall"""
@@ -28,6 +34,15 @@ class Boxs:
             self.__botPos[object] = pos
     def getBoxClass(self,pos:tuple[int,int]):
         return self.__Boxs[pos[0]][pos[1]]['class']
+    def getBoxINFO(self,pos:tuple[int,int],info=None):
+        """g,h,f"""
+        return self.__Boxs[pos[0]][pos[1]][info]
+    def setBoxINFO(self,pos:tuple[int,int],info=None,value=None):
+        """g,h,f"""
+        self.__Boxs[pos[0]][pos[1]][info] = value
+    def getBoxRect(self,pos:tuple[int,int]) -> pygame.Rect:
+        """return rect"""
+        return self.__Boxs[pos[0]][pos[1]]['rect']
 
     def update(self):
         for box in self.__Boxs:
@@ -46,11 +61,51 @@ class Bot:
     def __init__(self,screen:pygame.Surface,grid:Boxs):
         print("영우 배달 시작!")
         self.__screen = screen
-        self.__Pos = grid.getBoxCenter(grid.botPos["start"])
+        self.__grid = grid
+        self.__bottomLeft = (screen.get_height()//grid.size-1,0)
+        self.__topRight = (0,screen.get_width()//grid.size-1)
+        print(self.__bottomLeft,self.__topRight)
+
+        self.__start_pos:tuple[int,int] = grid.botPos["start"]
+        self.__end_pos:tuple[int,int] = grid.botPos["end"]
+        self.Open = [self.__start_pos]
+        self.__cur = None
+        self.Close = []
     
     def update(self):
-        pass
+        # print(self.Open,self.Close)
+        self.__cur = self.Open[0]
+        for i in range(len(self.Open)):
+            if self.__grid.getBoxINFO(self.Open[i],"f") <= self.__grid.getBoxINFO(self.__cur,"f") and self.__grid.getBoxINFO(self.Open[i],"h") < self.__grid.getBoxINFO(self.__cur,"h"):
+                self.__cur = self.Open[i]
+        self.Open.remove(self.__cur)
+        self.Close.append(self.__cur)
 
+        if self.__cur == self.__end_pos:
+            print ("영우 배달 완료!")
+            return
+        
+        print(self.__cur,"---")
+        cur_y ,cur_x= self.__cur
+        self.OpenListadd(cur_x,cur_y-1)
+        self.OpenListadd(cur_x+1,cur_y)
+        self.OpenListadd(cur_x,cur_y+1)
+        self.OpenListadd(cur_x-1,cur_y)
+        
+    def OpenListadd(self,checkX,checkY):
+        print("함수 실행",checkY,checkX,self.__bottomLeft,self.__topRight,end=" ")
+        if checkX >= self.__bottomLeft[1] and checkX < self.__topRight[1] and checkY <= self.__bottomLeft[0] and checkY >= self.__topRight[0] and not self.__grid.getBoxClass((checkY,checkX)) == "wall":
+            # NeighborNode = (checkY-self.__bottomLeft[0],checkX-self.__bottomLeft[1])
+            NeighborNode = (checkY,checkX)
+            print("이웃노드 활성화",NeighborNode,end=" ")
+            moveCost =self.__grid.getBoxINFO(self.__cur,'g') + (10 if self.__grid.getBoxRect(self.__cur).x - checkX==0 or self.__grid.getBoxRect(self.__cur).y - checkY==0 else 14)
+            if moveCost < self.__grid.getBoxINFO(NeighborNode,'g') or self.Open not in NeighborNode:
+                self.__grid.setBoxINFO(NeighborNode,'g',moveCost)
+                self.__grid.setBoxINFO(NeighborNode,'h',(abs(NeighborNode[1] - self.__end_pos[1]) + abs(NeighborNode[0] - self.__end_pos[0])) * 10)
+                self.__grid.setBoxINFO(NeighborNode,'perant',self.__cur)
+                self.__grid.setBoxINFO(NeighborNode,'f',(self.__grid.getBoxINFO(NeighborNode,'g')+self.__grid.getBoxINFO(NeighborNode,'h')))
+                self.Open.append(NeighborNode)
+        print()
 class Game:
     def __init__(self):
         pygame.init()
@@ -58,9 +113,12 @@ class Game:
         pygame.display.set_caption("영우 배달 게임")
         self.grid=Boxs(self.screen)
         
-        for _ in range(7):
-            randomPos = (random.randrange(0,self.screen.get_height()//100-1),random.randrange(0,self.screen.get_width()//100-1))
-            self.grid.setBoxClass(randomPos,"wall")
+        # for _ in range(7):
+        #     randomPos = (random.randrange(0,self.screen.get_height()//100-1),random.randrange(0,self.screen.get_width()//100-1))
+        #     self.grid.setBoxClass(randomPos,"wall")
+        set_wall = [(5,0),(4,0),(4,1),(4,3),(3,1),(3,3),(2,1),(1,2),(0,3),(6,2),(6,3)]
+        for wall in set_wall:
+            self.grid.setBoxClass(wall,"wall")
         self.grid.setBoxClass((6,0),"start")
         self.grid.setBoxClass((0,4),"end")
 
